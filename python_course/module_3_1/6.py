dddfe1e4a24bfae1be36f70a0016c3bdc6caaf88
def <armyuser90151@gmail.com>
2021-07-01 15:47:05 +0300

    add 28-31 modules 24-31 answers fix reamdme

---
+++ python_course/module_3_1/6.py
@@ -0,0 +1,16 @@
+# Задача 6. Web scraping
+# Дан вот такой несложный пример HTML-страницы.
+#
+#
+#
+# Изучите код этой страницы и реализуйте программу, которая получает список всех подзаголовков сайта (они заключены в теги h3).
+#
+#
+#
+# Ожидаемый результат:
+#
+# ['CONTENTS', '1. Creating a Web Page', '2. HTML Syntax', '3. Special Characters', '4. Converting Plain Text to HTML', '5. Effects', '6. Lists', '7. Links', '8. Tables', '9. Viewing Your Web Page', '10. Installing Your Web Page on the Internet', '11. Where to go from here', '12. Postscript: Cell Phones']
+#
+#
+#
+# Сделайте так, чтобы программа работала для любого сайта, где присутствуют такие теги.
